cc67f283517b5f1a871addd4043b6f5515146927
Vinicius José <viniciusjosedev@gmail.com>
2024-09-30 21:47:33 -0300

    feat: add compose with Dockerfile and structure auth module

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,9 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/index.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.NODE_PORT || 8080);
 }
 bootstrap();

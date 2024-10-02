6dd12edfec3ae9eb9718eb70f6caa083905b311b
Vinicius José <viniciusjosedev@gmail.com>
2024-10-02 05:57:27 -0300

    feat: add firebaseConfig for test

--- backend/src/firebase/config.ts
+++ backend/src/firebase/config.ts
@@ -3,9 +3,18 @@ import { initializeApp } from 'firebase/app';
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_APP_KEY,
   projectId: process.env.FIREBASE_PROJECT_ID,
-  databaseURL: process.env.FIREBASE_APP_KEY,
+  databaseURL: process.env.DATABASE_URL,
 };
 
-const configApp = initializeApp(firebaseConfig);
+const firebaseConfigTest = {
+  ...firebaseConfig,
+  databaseURL: process.env.TEST_DATABASE_URL,
+};
+
+const NODE_ENV = process.env.NODE_ENV;
+
+const configApp = initializeApp(
+  NODE_ENV === 'test' ? firebaseConfigTest : firebaseConfig,
+);
 
 export default configApp;

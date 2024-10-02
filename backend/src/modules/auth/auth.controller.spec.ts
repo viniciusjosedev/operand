69b65a7dcf71aa1beb03d738b829b598db97306c
Vinicius José <viniciusjosedev@gmail.com>
2024-10-02 05:59:01 -0300

    feat: add unit test for auth controller and auth service

--- backend/src/modules/auth/auth.controller.spec.ts
+++ backend/src/modules/auth/auth.controller.spec.ts
@@ -1,18 +1,68 @@
+import 'dotenv/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UserModule } from '../users/user.module';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [UserModule],
+      providers: [AuthService, JwtService],
       controllers: [AuthController],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should have a method singIn', () => {
+    expect(controller.signIn).toBeDefined();
+  });
+
+  it('should have a method singUp', () => {
+    expect(controller.signUp).toBeDefined();
+  });
+
+  it('should return a value from singIn', async () => {
+    const authServiceSpy = jest
+      .spyOn(authService, 'signIn')
+      .mockImplementation(() => Promise.resolve({ access_token: 'token' }));
+
+    expect(
+      await controller.signIn({
+        email: 'test@gmail.com',
+        password: '123456',
+      }),
+    ).toEqual({
+      access_token: 'token',
+    });
+
+    expect(authServiceSpy).toHaveBeenCalled();
+    expect(authServiceSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return a value from singUp', async () => {
+    const authServiceSpy = jest
+      .spyOn(authService, 'signUp')
+      .mockImplementation(() => Promise.resolve());
+
+    expect(
+      await controller.signUp({
+        email: 'test@gmail.com',
+        password: '123456',
+      }),
+    ).toEqual(undefined);
+
+    expect(authServiceSpy).toHaveBeenCalled();
+    expect(authServiceSpy).toHaveBeenCalledTimes(1);
   });
 });
--- backend/src/modules/auth/auth.service.spec.ts
+++ backend/src/modules/auth/auth.service.spec.ts
@@ -1,18 +1,73 @@
+import 'dotenv/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UserModule } from '../users/user.module';
+import { UserService } from '../users/user.service';
+import { AuthController } from './auth.controller';
+
+const EMAIL_MOCK = 'test@example.com';
+const PASSWORD_MOCK = '123456';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let userService: UserService;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [UserModule],
+      providers: [AuthService, JwtService],
+      controllers: [AuthController],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    userService = module.get<UserService>(UserService);
+    jwtService = module.get<JwtService>(JwtService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should have a method signIn', () => {
+    expect(service.signIn).toBeDefined();
+  });
+
+  it('should have a method signUp', () => {
+    expect(service.signUp).toBeDefined();
+  });
+
+  it('should return a value from signIn', async () => {
+    const userServiceSpy = jest
+      .spyOn(userService, 'findOne')
+      .mockImplementation(() =>
+        Promise.resolve({ email: EMAIL_MOCK, id: '1' }),
+      );
+
+    const jwtServiceSpy = jest
+      .spyOn(jwtService, 'sign')
+      .mockImplementation(() => 'token');
+
+    expect(await service.signIn(EMAIL_MOCK, PASSWORD_MOCK)).toEqual({
+      access_token: 'token',
+    });
+    expect(userServiceSpy).toHaveBeenCalled();
+    expect(userServiceSpy).toHaveBeenCalledTimes(1);
+    expect(jwtServiceSpy).toHaveBeenCalled();
+    expect(jwtServiceSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return a value from signUp', async () => {
+    const userServiceSpy = jest
+      .spyOn(userService, 'create')
+      .mockImplementation(() =>
+        Promise.resolve({ email: EMAIL_MOCK, id: '1' }),
+      );
+
+    expect(await service.signUp(EMAIL_MOCK, PASSWORD_MOCK)).toBeUndefined();
+
+    expect(userServiceSpy).toHaveBeenCalled();
+    expect(userServiceSpy).toHaveBeenCalledTimes(1);
   });
 });
